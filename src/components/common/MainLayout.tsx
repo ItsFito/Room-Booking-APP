571f1d1e897e67b43df8ad42630547d8370c4db8
Azzam Alfatih <azzam.alfatih065@gmail.com>
2025-12-04 14:12:17 +0700

    Adjust padding in MainLayout component

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -6,7 +6,7 @@ import { BottomNavigation } from "./BottomNavigation";
 export function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <main className="pb-24">{children}</main>
+      <main className="pb-14 sm:pb-16">{children}</main>
       <BottomNavigation />
     </div>
   );
